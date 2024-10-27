6b25299bac944b94985a5b1ce8d97a7f67149a6a
godfather-coder <sandrokika74@gmail.com>
2024-10-28 02:14:24 +0400

    changing Exception handling in AuthenticationController,ConnectionFeeController and GlobalExceptionHandler. add DivideException for handling division in ConnectionFee model.

--- src/main/java/com/example/mssqll/utiles/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/mssqll/utiles/exceptions/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
